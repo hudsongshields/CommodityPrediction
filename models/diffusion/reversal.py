89dc40a0fdcf7a34a74dd11da28dc3ed397707b9
hudsongshields <hudsongshields@gmail.com>
2026-02-19 12:34:43 -0500

    Added reversal test assignment

--- models/diffusion/reversal.py
+++ models/diffusion/reversal.py
@@ -2,3 +2,7 @@
 def reversal(model, x, t_max):
     # TODO: implement the reversal process to generate denoised sample from noisy input x at time step t_max
     pass
+
+if __name__ == '__main__':
+    # TODO: test the reversal function with a noisy image (see how image is denoised with decreasing time steps)
+    pass
