9cc05984fce6cf7b9fed2ff42dafc9d074772c6a
hudsongshields <hudsongshields@gmail.com>
2026-02-19 15:20:37 -0500

    added noise scheduler

--- models/diffusion/forward.py
+++ models/diffusion/forward.py
@@ -1,7 +1,13 @@
+import torch
 
-def noise_schedule(t: int, T: int) -> float:
+def noise_schedule(t: int, T: int, schedule: str = "cosine", offset: float = 5e-3) -> float:
     # TODO: implement a noise schedule function that returns the amount of noise to add at time step t
-    pass
+    if schedule == "cosine":
+        return torch.sin( (((t + offset) / T) / (1 + offset)) * (torch.pi / 2)) ** 2
+    if schedule == "linear":
+        return t / T
+    else:
+        raise ValueError(f"Unsupported noise schedule: {schedule}")
 
 def forward(x, T, t_max, noise_schedule: callable[[int, int], float], one_shot: bool=False):
     # T_max = int(pct_T * T) -> stop prematurely
