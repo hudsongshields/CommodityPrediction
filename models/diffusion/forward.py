0b62d3010e553b13344079a4576da12fed15dacb
Ezra Linnan <ezralinnan@gmail.com>
2026-02-26 14:33:56 -0500

    base forward process, one shot

--- models/diffusion/forward.py
+++ models/diffusion/forward.py
@@ -1,14 +1,30 @@
+import torch
 
-def noise_schedule(t: int, T: int) -> float:
-    # TODO: implement a noise schedule function that returns the amount of noise to add at time step t
-    pass
+def noise_schedule(t: int, T: int, beta_start=1e-4, beta_end=0.02) -> float:
+    return beta_start + (beta_end - beta_start) * (t / (T - 1))
 
-def forward(x, T, t_max, noise_schedule: callable[[int, int], float], one_shot: bool=False):
+def forward(x, T, t_max, noise_schedule: callable[[int, int], float]):
     # T_max = int(pct_T * T) -> stop prematurely
-    # TODO: add noise to x according to the forward diffusion process for t<T time steps
-    pass
+    device = x.device
+
+    # Build beta schedule
+    betas = torch.tensor(
+        [noise_schedule(t, T) for t in range(T)],
+        device=device
+    )
+
+    alphas = 1.0 - betas
+    alpha_bar = torch.cumprod(alphas, dim=0)
+
+    # direct sampling
+    noise = torch.randn_like(x)
+
+    sqrt_alpha_bar = torch.sqrt(alpha_bar[t_max])
+    sqrt_one_minus_alpha_bar = torch.sqrt(1 - alpha_bar[t_max])
+
+    x_t = sqrt_alpha_bar * x + sqrt_one_minus_alpha_bar * noise
+    return x_t
 
 
 if __name__ == '__main__':
-    # TODO: test the forward function with an image (see how gaussian noise is added to the image with increasing time steps/one shot)
     pass
