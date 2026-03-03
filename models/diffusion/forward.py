9a1dd181ec3517d76bc614faa49fd0c98115923e
hudsongshields <hudsongshields@gmail.com>
2026-03-03 12:57:08 -0500

    Merging Ezra's version of reversal to dev branch, this version of reversal expects that the model outputs noise rather than directly outputting score, which allows for a simpler and more intuitive reversal implementation

--- models/diffusion/forward.py
+++ models/diffusion/forward.py
@@ -1,20 +1,56 @@
 import torch
+import matplotlib.pyplot as plt
+from typing import Callable
+import torchvision
+import torchvision.transforms as transforms
 
-def noise_schedule(t: int, T: int, schedule: str = "cosine", offset: float = 5e-3) -> float:
-    # TODO: implement a noise schedule function that returns the amount of noise to add at time step t
-    if schedule == "cosine":
-        return torch.sin( (((t + offset) / T) / (1 + offset)) * (torch.pi / 2)) ** 2
-    if schedule == "linear":
-        return t / T
-    else:
-        raise ValueError(f"Unsupported noise schedule: {schedule}")
+## continuous sigma(t) schedule, exponential growth
+def noise_schedule(t: int, T: int, sigma_min=0.01, sigma_max=250.0) -> float:
+    return sigma_min * (sigma_max - sigma_min) ** (t / T)
 
-def forward(x, T, t_max, noise_schedule: callable[[int, int], float], one_shot: bool=False):
-    # T_max = int(pct_T * T) -> stop prematurely
-    # TODO: add noise to x according to the forward diffusion process for t<T time steps
-    pass
+def forward(x, T, t_max, noise_schedule: Callable[[int, int], float]):
+    # t_max = int(pct_T * T) -> stop prematurely
+    device = x.device
 
+    # noise based on the timestep
+    sigma_t = noise_schedule(t_max, T)
+    sigma_t = torch.tensor(sigma_t, device=device, dtype=x.dtype)
 
+    # random noise, scale, then add to image
+    noise = torch.randn_like(x)
+    x_t = x + sigma_t * noise
+    return x_t
+
+# test data
 if __name__ == '__main__':
-    # TODO: test the forward function with an image (see how gaussian noise is added to the image with increasing time steps/one shot)
-    pass
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    dataset = torchvision.datasets.FashionMNIST(
+        root="./data",
+        train=True,
+        download=True,
+        transform=transform
+    )
+
+    loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+
+    x, label = next(iter(loader))
+    print(x.shape)
+
+
+    T = 1000
+
+    for t in [10, 100, 300, 500, 700, 999]:
+        x_noisy = forward(x, T, t, noise_schedule)
+
+        # scaling
+        img = x_noisy.squeeze().detach().numpy()
+        img = (img - img.min()) / (img.max() - img.min())
+
+        plt.imshow(img, cmap='gray')
+        plt.title(f"t = {t}")
+        plt.axis("off")
+        plt.show()
